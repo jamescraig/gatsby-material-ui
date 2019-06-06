6da45e2f39f6b7c0271e24d5db901a6116a75762
James Craig <james.craig@gmail.com>
2019-06-05 20:40:26 -0400

    add test modal

--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -4,9 +4,21 @@ import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 import Berrylines from './berry-lines.svg';
+import Modal from '@material-ui/core/Modal';
+import Typography from '@material-ui/core/Typography';
 
 // const primary = deepPurple[800];
 const styles = theme => ({
+  modalPaper: {
+    position: 'absolute',
+    top: `50%`,
+    left: `50%`,
+    transform: `translate(-50%, -50%)`,
+    width: '80%',
+    maxWidth: '48em',
+    padding: '1em',
+    outline: 'none',
+  },
   paperChart: {
     margin: '0 auto',
     padding: '1em',
@@ -125,12 +137,21 @@ const styles = theme => ({
 
 function Timeline(props) {
   const { classes } = props;
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <React.Fragment>
       <Paper className={classes.paperChart} rounded={true} elevation="10">
         <img className={classes.svg} src={Berrylines} alt={'Berry Timeline'} />
-        <Button className={classes.salmonberry}>Salmonberry</Button>
+        <Button className={classes.salmonberry} onClick={handleOpen}>Salmonberry</Button>
         <Button className={classes.redhuckleberry}>Red Huckleberry</Button>
         <Button className={classes.redelderberry}>Red Elderberry</Button>
         <Button className={classes.thimbleberry}>Thimbleberry</Button>
@@ -144,6 +165,24 @@ function Timeline(props) {
         <Button className={classes.bogblueberry}>Bog Blueberry</Button>
         <Button className={classes.evergreenhuckleberry}>Evergreen Huckleberry</Button>
       </Paper>
+      <Modal
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+        open={open}
+        onClose={handleClose}
+      >
+        <Paper className={classes.modalPaper}>
+          <Typography variant="h6" id="modal-title">
+            Salmonberry
+          </Typography>
+          <Typography variant="subtitle1" id="simple-modal-description">
+            Rubus spectabilis
+          </Typography>
+          <Typography variant="body1">
+            Native to the west coast of North America from west central Alaska to California, inland as far as Idaho. Salmonberries are found in moist forests and stream margins, especially in the coastal forests. In open areas they often form large thickets, and thrive in the open spaces under stands of red alder (Alnus rubra).
+          </Typography>
+        </Paper>
+      </Modal>
     </React.Fragment>
   );
 }
